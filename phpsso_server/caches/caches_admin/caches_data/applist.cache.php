40b9452a95d2e2020a056a4ee8efd596ec6a0a94
Mark Zhao <zhaody901@126.com>
2013-06-14 22:59:36 +0800

    init files

---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://mall.sdinfo.cc/',
+    'authkey' => '1HyWtH9QoECkxWZWp0c60X04w6sTQqdt',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'cms9',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => 'mk_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		)
+);
+
+?>
